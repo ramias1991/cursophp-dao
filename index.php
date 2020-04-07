08ef24d2dc3a85f5660db2dfe702c07a5b300fb3
Ramias Souza <ramias1991@hotmail.com>
2020-04-06 23:13:26 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -6,7 +6,24 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
+
+/* ## Carrega um usuário
 $root = new Usuario();
 $root->loadById(2);
 echo $root;
+*/
+
+/* Carrega um lista de usuários
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/* Carrega uma lista através de uma busca pelo login
+$lista = Usuario::search("l");
+echo json_encode($lista);
+*/
+
+$usuario = new Usuario();
+$usuario->login("ramiassouza", "!@#$%");
+echo $usuario;
 ?>
